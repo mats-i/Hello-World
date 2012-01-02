83fa722313d97babaca3ee6b27832b1657b4a6a5
Mats Ingerdal <mats@agoodid.se>
2012-01-02 02:12:47 +0100

    Mer queen's english. Mindre redneck-artat.

--- hello.py
+++ hello.py
@@ -26,8 +26,7 @@ def main():
     name = sys.argv[1]
   else:
     name = 'World'
-  print 'Howdy', name
-  print 'Yay!'
+  print 'Hello', name
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
