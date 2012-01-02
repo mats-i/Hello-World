f806e05e89da62cfb074fc1ed764d87a47921f67
Mats Ingerdal <mats@agoodid.se>
2012-01-02 02:15:53 +0100

    Försvenskning: "Hej värld"

--- hello.py
+++ hello.py
@@ -21,12 +21,12 @@ import sys
 
 # Define a main() function that prints a little greeting.
 def main():
-  # Get the name from the command line, using 'World' as a fallback.
+  # Get the name from the command line, using 'värld' as a fallback.
   if len(sys.argv) >= 2:
     name = sys.argv[1]
   else:
-    name = 'World'
-  print 'Hello', name
+    name = 'värld'
+  print 'Hej', name
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
